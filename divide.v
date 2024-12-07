726a65faf0f18bd1d5d9671fbab7343cef194fbe
Vaddi Divya <vaddidivya2003@gmail.com>
2024-12-07 20:30:36 +0530

    divide.v

--- divide.v
+++ divide.v
@@ -1,4 +1,23 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/26/2024 11:47:57 AM
+// Design Name: 
+// Module Name: divide
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module divide (
     input  [31:0] a,        // Input floating-point number a
